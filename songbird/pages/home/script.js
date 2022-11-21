c064c0709a35ff1616d9411b5aa05d540e2fda57
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-11-21 11:26:42 +0300

    feat: add media queries Home page

--- songbird/pages/home/script.js
+++ songbird/pages/home/script.js
@@ -1,14 +1,19 @@
 //video change
 
-const video = document.getElementById("video");
+const video = document.getElementById('video');
 function randomInteger(min, max) {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
 function videoChande() {
-  let i = randomInteger(1, 16);
-  video.setAttribute('src', `../../assets/video/video${i}.mp4`);
+  if(window.matchMedia("(min-width: 450px)").matches) {
+    let i = randomInteger(1, 16);
+    video.setAttribute('src', `../../assets/video/video${i}.mp4`);
+  } else {
+    let i = randomInteger(17, 25);
+    video.setAttribute('src', `../../assets/video/video${i}.mp4`);
+  }
 }
 
 videoChande()
@@ -16,3 +21,32 @@ setInterval(() => {
   videoChande();
 }, 10000);
 
+//burger
+const blackout = document.querySelector('.blackout'),
+      burger = document.querySelector('.burger'),
+      body = document.querySelector('body'),
+      navList = document.querySelector('.nav__list'),
+      navLinks = document.querySelectorAll('.nav__link');
+
+function openMenu() {
+   blackout.classList.toggle('display-block');
+   burger.classList.toggle('burger-active');
+   navList.classList.toggle('nav__list-active');
+   body.classList.toggle('not-scroll');
+   for (let navLink of navLinks) {
+      navLink.classList.toggle('nav__link-active');
+   }
+}
+
+function closeMenu() {
+   blackout.classList.remove('display-block');
+   burger.classList.remove('burger-active');
+   navList.classList.remove('nav__list-active');
+   body.classList.remove('not-scroll');
+}
+
+burger.addEventListener('click', openMenu);
+blackout.addEventListener('click', closeMenu);
+for (let navLink of navLinks) {
+   navLink.addEventListener('click', closeMenu);
+}
