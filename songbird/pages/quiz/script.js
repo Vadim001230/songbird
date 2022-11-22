60731a1bc52594dae05422ff0f10cd22f2519460
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-11-22 05:50:49 +0300

    feat: add media queries quiz page

---
+++ songbird/pages/quiz/script.js
@@ -0,0 +1,28 @@
+// //Progress player
+// //volume
+// document.querySelector('.volume-input').oninput = audioVolume;
+// function audioVolume() {
+//   let v = this.value;
+//   audio.volume = v / 100;
+// }
+
+// const audioQuestion = document.getElementById('audioQuestion'),
+//       audioTrue = document.getElementById('audioTrue'),
+//       audioFalse = document.getElementById('audioFalse'),
+//       audioWin = document.getElementById('audioWin'),
+//       audioCard = document.getElementById('audioCard');
+
+
+
+// // function playSong() {
+// //   progressAudio.play();
+// //   pauseSrc.src = 'img/svg/pause.svg';
+// //   song__image.style.transform = "scale(1.15)";
+// // }
+
+// // function pauseSong() {
+// //   player.classList.remove('active');
+// //   audio.pause();
+// //   pauseSrc.src = 'img/svg/play.svg';
+// //   song__image.style.transform = "scale(1)";
+// // }
